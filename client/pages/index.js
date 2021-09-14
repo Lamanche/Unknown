a0f1cbd8468629b82acec12308a2fbbf58cc2d32
Andres <andressiirak@hotmail.com>
2021-09-14 20:42:30 +0300

    Unknown

---
+++ client/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Unknown</title>
+      </Head>
+
+      <main className='text-center overscroll-contain overflow-auto h-80 p-2 bg-gray-100'>
+        Tere
+      </main>
+    </>
+  );
+}
