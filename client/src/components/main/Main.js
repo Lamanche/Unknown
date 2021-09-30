ce8a778cdec53aad6162522a28d3f6c0ed52f57a
Andres <andressiirak@hotmail.com>
2021-09-30 20:20:17 +0300

    unknown

--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -1,34 +1,24 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useHistory } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import Card from "./Card";
 import Search from "./Search";
 import { getUser } from "../../api/users";
 import useGetData from "./utils/useGetData";
-import auth from "../../firebase";
 
 const Main = () => {
   const { signOut, currentUser } = useAuth();
   const history = useHistory();
-  //const { loading, error, apiData } = useGetData(getUser("andres"));
-  //console.log(auth.currentUser);
+  const { loading, error, apiData } = useGetData(getUser("andres"));
 
-  const user = async () => {
-    console.log(auth.currentUser);
+  /*const user = async () => {
     try {
       const data = await getUser("andres");
       console.log(data.data);
     } catch (error) {
       console.log(error);
     }
-  };
-
-  /*useEffect(() => {
-    //user();
-    //const data = getData(() => getUser("andres"));
-    //getData(() => getUser("andres"));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);*/
+  };*/
 
   const leave = async () => {
     try {
@@ -45,7 +35,7 @@ const Main = () => {
       <Search />
       <Card />
       <button onClick={leave}>Leave</button>
-      <button onClick={user}>Mingi nupp</button>
+      <button >Mingi nupp</button>
     </main>
   );
 };
--- client/src/components/main/utils/useGetData.js
+++ client/src/components/main/utils/useGetData.js
@@ -3,13 +3,13 @@ import { useState, useEffect } from "react";
 const useGetData = (url) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const { apiData, setApiData } = useState(null);
+  const [apiData, setApiData] = useState(null);
 
   const getData = async () => {
     setLoading(true);
     try {
       const res = await url;
-      //setApiData(res);
+      setApiData(res);
       setLoading(false);
       console.log(res.data);
     } catch (error) {
